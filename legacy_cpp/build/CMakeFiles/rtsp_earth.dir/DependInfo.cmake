65bab3d5a9b78704a0821bb6ef008f335654d3e0
ekrishgupta <ekrishgupta@users.noreply.github.com>
2026-02-18 01:52:14 -0800

    chore(init): initialize vite react project with react-globe.gl

---
+++ legacy_cpp/build/CMakeFiles/rtsp_earth.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/krishgupta/Projects/rtsp-earth/src/DataLoader.cpp" "CMakeFiles/rtsp_earth.dir/src/DataLoader.cpp.o" "gcc" "CMakeFiles/rtsp_earth.dir/src/DataLoader.cpp.o.d"
+  "/Users/krishgupta/Projects/rtsp-earth/src/Globe.cpp" "CMakeFiles/rtsp_earth.dir/src/Globe.cpp.o" "gcc" "CMakeFiles/rtsp_earth.dir/src/Globe.cpp.o.d"
+  "/Users/krishgupta/Projects/rtsp-earth/src/main.cpp" "CMakeFiles/rtsp_earth.dir/src/main.cpp.o" "gcc" "CMakeFiles/rtsp_earth.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ legacy_cpp/build/CMakeFiles/rtsp_earth.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for rtsp_earth.
+# This may be replaced when dependencies are built.
